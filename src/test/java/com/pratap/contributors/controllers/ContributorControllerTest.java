1e439e2d43991abdb2533653bfdcde8312e111b2
pratap_narayan <pratap_narayan@apple.com>
2020-07-10 18:06:29 +0530

    initial commit

---
+++ src/test/java/com/pratap/contributors/controllers/ContributorControllerTest.java
@@ -0,0 +1,18 @@
+package com.pratap.contributors.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+class ContributorControllerTest {
+
+	@InjectMocks
+	ContributorController contributorController;
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
