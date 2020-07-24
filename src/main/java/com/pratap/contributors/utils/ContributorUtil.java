d6c6cfde4fba657e0a9ad0f6d59312d96af51215
pratap_narayan <pratap_narayan@apple.com>
2020-07-24 18:20:27 +0530

    introduced supplier to create random ids

--- src/main/java/com/pratap/contributors/utils/ContributorUtil.java
+++ src/main/java/com/pratap/contributors/utils/ContributorUtil.java
@@ -2,26 +2,27 @@ package com.pratap.contributors.utils;
 
 import java.security.SecureRandom;
 import java.util.Random;
+import java.util.function.Supplier;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class ContributorUtil {
 
-	private final Random RANDOM = new SecureRandom();
-	private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	private final static Random RANDOM = new SecureRandom();
+	private final static String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	private static final int LENGTH = 8;
 	
-	public String generatedContributorId(int length) {
-		return generateRandomString(length);
-	}
-
-	private String generateRandomString(int length) {
-
-		StringBuilder returnVal = new StringBuilder(length);
+	
+	
+	public static Supplier<String> idGeneratedSupplier = () -> {
+	
+		StringBuilder returnVal = new StringBuilder(LENGTH);
 		
-		for(int i = 0; i < length; i++) {
+		for(int i = 0; i < LENGTH; i++) {
 			returnVal.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
 		}
 		return new String(returnVal);
-	}
+	};
+	
 }
